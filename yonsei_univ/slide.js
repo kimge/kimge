3e41b0f3e14e69d0c6f32698fb01416c8a8d80b6
kimgoeun <kiimgoeun@gmail.com>
2018-06-30 23:11:05 +0900

    Update file (slide.js)

---
+++ yonsei_univ/slide.js
@@ -0,0 +1,9 @@
+//Javascript로 연세대학교 slide 만들기
+var i = 0;
+var slideIndex = [
+  document.getElementById('slide1'),
+  document.getElementById('slide2'),
+  document.getElementById('slide3'),
+  document.getElementById('slide4')
+];
+console.log(slideIndex[0]);
